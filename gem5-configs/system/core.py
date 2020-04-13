2cebb089ffe0ee876bcc991f66f90eed18a423ba
Trivikram Reddy <49581726+tv-reddy@users.noreply.github.com>
2020-04-13 01:40:36 -0700

    Update delays and MAXCPU
    Updated delays after comparing against hardware

--- gem5-configs/system/core.py
+++ gem5-configs/system/core.py
@@ -1,7 +1,3 @@
-################################################################
-# This is a configuration file for front and back end of the cpu
-################################################################
-
 import m5
 from m5.objects import *
 
@@ -15,18 +11,6 @@ class IntMult(FUDesc):
     opList = [ OpDesc(opClass='IntMult', opLat=4) ]
     count = 1
 
-class IntDiv(FUDesc):
-    opList = [ OpDesc(opClass='IntDiv', opLat=20, pipelined=False) ]
-
-    # DIV and IDIV instructions in x86 are implemented using a loop which
-    # issues division microops.  The latency of these microops should really be
-    # one (or a small number) cycle each since each of these computes one bit
-    # of the quotient.
-    if buildEnv['TARGET_ISA'] in ('x86'):
-        opList[0].opLat=1
-
-    count=1
-
 class FP_ALU(FUDesc):
     opList = [ OpDesc(opClass='FloatAdd', opLat=3),
                OpDesc(opClass='FloatCmp', opLat=3),
@@ -37,11 +21,18 @@ class FP_Mult(FUDesc):
     opList = [ OpDesc(opClass='FloatMult', opLat=5),
                OpDesc(opClass='FloatMultAcc', opLat=5),
                OpDesc(opClass='FloatMisc', opLat=3) ]
-    count = 2
+    count = 3
 
-class FP_Div(FUDesc):
-    opList = [ OpDesc(opClass='FloatDiv', opLat=15, pipelined=False),
-               OpDesc(opClass='FloatSqrt', opLat=15, pipelined=False) ]
+class DivUnit(FUDesc):
+    # DIV and IDIV instructions in x86 are implemented using a loop which
+    # issues division microops.  The latency of these microops should really be
+    # one (or a small number) cycle each since each of these computes one bit
+    # of the quotient.
+    opList = [ OpDesc(opClass='IntDiv', opLat=1, pipelined=False),
+                # OpDesc(opClass='FloatDiv', opLat=15, pipelined=False),
+                # OpDesc(opClass='FloatSqrt', opLat=15, pipelined=False),
+                OpDesc(opClass='SimdFloatDiv', opLat=15, pipelined=False),
+                OpDesc(opClass='SimdFloatSqrt', opLat=10, pipelined=False) ]
     count = 1
 
 class SIMD_Unit(FUDesc):
@@ -50,45 +41,39 @@ class SIMD_Unit(FUDesc):
                OpDesc(opClass='SimdAlu', opLat=1),
                OpDesc(opClass='SimdCmp', opLat=1),
                OpDesc(opClass='SimdCvt', opLat=3),
-               OpDesc(opClass='SimdMisc', opLat=3),
                OpDesc(opClass='SimdMult', opLat=5),
                OpDesc(opClass='SimdMultAcc', opLat=5),
                OpDesc(opClass='SimdShift', opLat=2),
                OpDesc(opClass='SimdShiftAcc', opLat=2),
                OpDesc(opClass='SimdSqrt', opLat=4),
-               OpDesc(opClass='SimdFloatAdd', opLat=3),
-               OpDesc(opClass='SimdFloatAlu', opLat=3),
-               OpDesc(opClass='SimdFloatCmp', opLat=3),
+               OpDesc(opClass='SimdFloatAdd', opLat=4),
+               OpDesc(opClass='SimdFloatAlu', opLat=4),
+               OpDesc(opClass='SimdFloatCmp', opLat=4),
                OpDesc(opClass='SimdFloatCvt', opLat=4),
-               OpDesc(opClass='SimdFloatDiv', opLat=15, pipelined=False),
-               OpDesc(opClass='SimdFloatMisc', opLat=3),
-               OpDesc(opClass='SimdFloatMult', opLat=5),
-               OpDesc(opClass='SimdFloatMultAcc', opLat=6),
-               OpDesc(opClass='SimdFloatSqrt', opLat=10, pipelined=False) ]
-    count = 4
-
-class ReadPort(FUDesc):
-    opList = [ OpDesc(opClass='MemRead'),
-               OpDesc(opClass='FloatMemRead') ]
+                ]
     count = 2
 
-class WritePort(FUDesc):
-    opList = [ OpDesc(opClass='MemWrite'),
-               OpDesc(opClass='FloatMemWrite') ]
+class SIMD_MUL(FUDesc):
+    opList = [ OpDesc(opClass='SimdFloatMult', opLat=5),
+               OpDesc(opClass='SimdFloatMultAcc', opLat=5) ]
+    count = 2
+
+class SIMD_Misc(FUDesc):
+    opList = [ OpDesc(opClass='SimdMisc', opLat=3),
+               OpDesc(opClass='SimdFloatMisc', opLat=3), ]
     count = 1
 
-class RdWrPort(FUDesc):
-    opList = [ OpDesc(opClass='MemRead'), OpDesc(opClass='MemWrite'),
-               OpDesc(opClass='FloatMemRead'), OpDesc(opClass='FloatMemWrite')]
-    count = 0
+class FPMem(FUDesc):
+    opList = [ OpDesc(opClass='FloatMemRead', opLat=1),
+               OpDesc(opClass='FloatMemWrite', opLat=1) ]
+    count = 2
 
-class IprPort(FUDesc):
-    opList = [ OpDesc(opClass='IprAccess', opLat = 3, pipelined = False) ]
-    count = 1
+# class IprPort(FUDesc):
+#     opList = [ OpDesc(opClass='IprAccess', opLat = 3, pipelined = False) ]
+#     count = 1
 
 class Ideal_FUPool(FUPool):
-    FUList = [ IntALU(), IntMult(), IntDiv(), FP_ALU(), FP_Mult(), FP_Div(), ReadPort(),
-               SIMD_Unit(), WritePort(), RdWrPort(), IprPort() ]
+    FUList = [ IntALU(), IntMult(), DivUnit(), SIMD_Unit(), SIMD_MUL(), SIMD_Misc(), FPMem() ]
 
 
 class UnCalibCPU(DerivO3CPU):
@@ -148,14 +133,13 @@ class CalibCPU(DerivO3CPU):
     # Front End
     ######################################
     branchPred = LTAGE()
-
     # Pipeline widths
     fetchWidth = 7
     decodeWidth = 7
 
     # Pipeline delays
-    fetchToDecodeDelay = 2
-    decodeToRenameDelay = 3
+    # fetchToDecodeDelay = 1
+    # decodeToRenameDelay = 1
 
     fetchBufferSize = 16
     fetchQueueSize = 64
@@ -166,8 +150,11 @@ class CalibCPU(DerivO3CPU):
     ######################################
 
     fuPool = Ideal_FUPool()
-    fuPool.FUList[0].count = 6
-
+    fuPool.FUList[0].count = 7
+    fuPool.FUList[0].count = 4
+    fuPool.FUList[1].opList[0].opLat = 2
+    fuPool.FUList[4].opList[0].opLat = 4
+    # fuPool.FUList.append(FP_ALU())
     # Pipeline widths
     renameWidth = 7
     dispatchWidth = 7
@@ -177,13 +164,13 @@ class CalibCPU(DerivO3CPU):
     squashWidth = 7
 
     # Pipeline delays
-    renameToIEWDelay = 4
-    issueToExecuteDelay  = 1
-    iewToRenameDelay = 1
-    iewToCommitDelay = 4
-    commitToFetchDelay = 1
-    commitToIEWDelay = 1
-    commitToRenameDelay = 1
+    # renameToIEWDelay = 1
+    # issueToExecuteDelay  = 1
+    # iewToRenameDelay = 1
+    # iewToCommitDelay = 1
+    # commitToFetchDelay = 1
+    # commitToIEWDelay = 1
+    # commitToRenameDelay = 1
 
     LQEntries = 72
     SQEntries = 56
@@ -206,9 +193,9 @@ class MaxCPU(DerivO3CPU):
     fetchToDecodeDelay = 1
     decodeToRenameDelay = 1
 
-    fetchBufferSize = 16
-    fetchQueueSize = 64
-    numIQEntries = 64
+    # fetchBufferSize = 16
+    fetchQueueSize = 256
+    numIQEntries = 128
 
     ######################################
     # Back End
@@ -220,9 +207,8 @@ class MaxCPU(DerivO3CPU):
     fuPool.FUList[2].count = 32
     fuPool.FUList[3].count = 32
     fuPool.FUList[4].count = 32
-    fuPool.FUList[5].count = 32
-    fuPool.FUList[6].count = 32
-    fuPool.FUList[7].count = 32
+    # fuPool.FUList[5].count = 32
+    # fuPool.FUList[6].count = 32
 
     # Pipeline widths
     renameWidth = 32
@@ -241,8 +227,9 @@ class MaxCPU(DerivO3CPU):
     commitToIEWDelay = 1
     commitToRenameDelay = 1
 
-    LQEntries = 72
-    SQEntries = 56
-    numPhysIntRegs = 180
-    numPhysFloatRegs = 168 # Need to change this
-    numROBEntries = 224
+    LQEntries = 128
+    SQEntries = 128
+    numPhysIntRegs = 256
+    numPhysFloatRegs = 256 # Need to change this
+    numROBEntries = 2096
+    
